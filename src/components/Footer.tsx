6e4985311a9630f96a0be3a40689e05ed188fbd5
Samuel <68117536+SamuelBenedix@users.noreply.github.com>
2025-07-07 16:08:50 +0700

    feature : componets

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,33 +2,18 @@
 export default function Footer() {
   return (
     <footer className="content-footer footer bg-footer-theme">
-      <div className="container-xxl d-flex justify-content-between py-4">
-        <div>
-          © {new Date().getFullYear()}, made with ❤️ by{' '}
-          <a href="https://themeselection.com">ThemeSelection</a>
-        </div>
-        <div>
-          <a
-            href="https://themeselection.com/license/"
-            className="footer-link me-4"
-          >
-            License
-          </a>
-          <a href="https://themeselection.com/" className="footer-link me-4">
-            More Themes
-          </a>
-          <a
-            href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/"
-            className="footer-link me-4"
-          >
-            Docs
-          </a>
-          <a
-            href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
-            className="footer-link"
-          >
-            Support
-          </a>
+      <div className="container-xxl">
+        <div className="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+          <div className="text-body">
+            ©, made with ❤️ by
+            <a
+              href="https://themeselection.com"
+              target="_blank"
+              className="footer-link"
+            >
+              ThemeSelection
+            </a>
+          </div>
         </div>
       </div>
     </footer>
