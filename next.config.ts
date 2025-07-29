b24c0713489f4af7cb51524a4dcb2a83ebcee2dc
Samuel <68117536+SamuelBenedix@users.noreply.github.com>
2025-07-29 10:53:15 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import webpack from 'webpack'; // Import webpack
 
 const nextConfig: NextConfig = {
+  output: 'standalone', // ✅ Tambahkan ini
   images: {
     domains: ["img.freepik.com", "veterinaire-tour-hassan.com"],
   },
